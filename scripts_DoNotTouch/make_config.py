296d405995496adb37d13c1badb515951c2f0386
Raditya Utama <proteusds@yahoo.co.id>
2023-12-13 17:54:31 -0500

    Update make_config.py: Add option to use previous config parameters
    Add option to use previous config parameters

--- scripts_DoNotTouch/make_config.py
+++ scripts_DoNotTouch/make_config.py
@@ -1,8 +1,20 @@
 
 def config():
-    print("Provide any unique project name:")
-    print("\033[91m"+"If you want to use our example dataset, type"+"\033[94m"+" example_dataset"+"\x1b[0m")
-    project_name = input()
-    conf = open("../scripts_DoNotTouch/config.py", "w")
-    conf.write("project_name="+"'"+project_name+"'")
-    conf.close()
+    print("Do you want to use your most recent project name, genome, design matrix, and reads folders:(e.g y/n)")
+    param = input()
+    if param == 'n':
+        print("========================================")
+        print("Provide any unique project name:")
+        print("\033[91m"+"If you want to use our example dataset, type"+"\033[94m"+" example_dataset"+"\x1b[0m")
+        project_name = input()
+        conf = open("../scripts_DoNotTouch/config.py", "w")
+        conf.write("project_name="+"'"+project_name+"'"+"\n")
+        conf.write("parameters_exist="+"'"+param+"'"+"\n")
+        conf.close()
+    else:
+        with open('../scripts_DoNotTouch/config.py', 'r') as file:
+            data = file.readlines()
+        data[1] = "parameters_exist="+"'"+param+"'"+"\n"
+        with open('../scripts_DoNotTouch/config.py', 'w') as file:
+            file.writelines( data )
+            
