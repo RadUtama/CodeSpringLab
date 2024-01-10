a34de22cf9150a26497ad4a361a35d2db7b80c88
Raditya Utama <proteusds@yahoo.co.id>
2024-01-09 19:30:20 -0500

    Delete scripts_DoNotTouch/cutadapt_PE directory
    Delete scripts_DoNotTouch/cutadapt_PE directory

--- scripts_DoNotTouch/cutadapt_PE/qsub_cutadapt_PE.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-qsub -l m_mem_free=2G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_cutadapt.txt -e ../../csl_results/${7}/log/error_cutadapt.txt -V ../scripts_DoNotTouch/cutadapt_PE/cutadapt_PE.sh $1 $2 $3 $4 $5 $6
-
