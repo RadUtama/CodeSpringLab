857c5f7c6e077a9f0fe37f70f0392c3419f16a32
Raditya Utama <proteusds@yahoo.co.id>
2024-01-12 00:42:39 -0500

    Add files via upload

---
+++ scripts_DoNotTouch/Homer/qsub_homer_diffpeak.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+qsub  -l m_mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_homer_diffpeak.txt -e ../../csl_results/${7}/log/error_homer_diffpeak.txt homer_diffpeak.sh $1 $2 $3 $4 $5 $6
+
+
