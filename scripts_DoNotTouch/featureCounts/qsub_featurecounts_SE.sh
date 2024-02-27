8dcf99e2f4b9639e5b46c9702b5c8628cfb01b1c
Raditya Utama <proteusds@yahoo.co.id>
2024-02-26 22:50:34 -0500

    Update qsub_featurecounts_SE.sh: strandBED include
    strandBED include

--- scripts_DoNotTouch/featureCounts/qsub_featurecounts_SE.sh
+++ scripts_DoNotTouch/featureCounts/qsub_featurecounts_SE.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_featurecounts.txt -e ../../csl_results/${5}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_SE.sh $1 $2 $3 $4
-
+qsub -l m_mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${6}/log/output_featurecounts.txt -e ../../csl_results/${6}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_SE.sh $1 $2 $3 $4 $5
