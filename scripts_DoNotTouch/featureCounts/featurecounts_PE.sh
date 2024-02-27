fe9c3ce4bdca7a811a095d90a6ded38b4c9b00df
Raditya Utama <proteusds@yahoo.co.id>
2024-02-26 22:45:23 -0500

    Update featurecounts_PE.sh: Strand estimate rseqc
    Strand estimate rseqc

--- scripts_DoNotTouch/featureCounts/featurecounts_PE.sh
+++ scripts_DoNotTouch/featureCounts/featurecounts_PE.sh
@@ -1,4 +1,22 @@
 
+/grid/bsr/home/utama/.local/bin/infer_experiment.py -r $5 -i $1 > $4_strand.txt
+
+fw=$(awk 'NR==5 {print $NF}' $4_strand.txt)
+rv=$(awk 'NR==6 {print $NF}' $4_strand.txt)
+
+diff=$(echo "scale=3 ;($fw - $rv)*100/($fw + $rv)" | bc)
+
+if (( $(echo "${diff#-} < 10" | bc -l) ));then
+        strand=unstranded
+        strand_idx=0
+elif (( $(echo "$diff > 0" | bc -l) ));then
+        strand=forward
+        strand_idx=1
+else
+        strand=reverse
+        strand_idx=2
+fi
+
 module load EBModules
 module load Subread
 
@@ -9,7 +27,7 @@ featureCounts -a $2 \
         -p --countReadPairs \
         -t "exon" \
         -g $3 \
-        -s 0 \
+        -s ${strand_idx} \
         -Q 12 \
         --minOverlap 1 \
         -C \
