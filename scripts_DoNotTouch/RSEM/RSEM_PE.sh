f4f622ddfce551ad70e9d6f1fa378d67dc9bc0a6
Raditya Utama <proteusds@yahoo.co.id>
2024-03-11 11:08:15 -0400

    Add RSEM
    Add RSEM

---
+++ scripts_DoNotTouch/RSEM/RSEM_PE.sh
@@ -0,0 +1,37 @@
+
+
+fw=$(awk 'NR==5 {print $NF}' $4_strand.txt)
+rv=$(awk 'NR==6 {print $NF}' $4_strand.txt)
+
+diff=$(echo "scale=3 ;($fw - $rv)*100/($fw + $rv)" | bc)
+
+if (( $(echo "${diff#-} < 50" | bc -l) ));then
+        strand=none
+        strand_idx=0
+elif (( $(echo "$diff > 0" | bc -l) ));then
+        strand=forward
+        strand_idx=1
+else
+        strand=reverse
+        strand_idx=2
+fi
+
+############ RSEM ################
+
+module load EBModules
+module load STAR/2.7.10a-GCC-10.3.0
+module load RSEM/1.3.3-foss-2019b
+module load SAMtools/1.16.1-GCC-11.3.0
+
+ulimit -n 10000
+
+rsem-calculate-expression --paired-end --strandedness ${strand} \
+	--bam $6 \
+	$2 \
+	$4
+
+rm -rf ${4}.transcript.bam
+rm -rf ${4}.stat
+
+#################################
+
---
+++ scripts_DoNotTouch/RSEM/qsub_RSEM_SE.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qsub -l m_mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_rsem.txt -e ../../csl_results/${7}/log/error_rsem.txt -V ../scripts_DoNotTouch/RSEM/RSEM_SE.sh $1 $2 $3 $4 $5 $6
