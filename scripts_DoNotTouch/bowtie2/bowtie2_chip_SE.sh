c73f368284507d0dbde8182cd7871bf3d242f224
Raditya Utama <proteusds@yahoo.co.id>
2024-12-18 16:10:43 -0500

    Update bowtie2_chip_SE.sh: Remove sortedByName bam,use sortedByCoord for removeDup,--extendReads 200
    Remove sortedByName bam,use sortedByCoord for removeDup,--extendReads 200

--- scripts_DoNotTouch/bowtie2/bowtie2_chip_SE.sh
+++ scripts_DoNotTouch/bowtie2/bowtie2_chip_SE.sh
@@ -23,17 +23,28 @@ samtools view -h -bS -o ${1}Aligned.sortedByName.out.bam ${1}Aligned.sortedByNam
 rm ${1}Aligned.sortedByName.out.sam
 
 samtools sort -o ${1}Aligned.sortedByCoord.out.bam ${1}Aligned.sortedByName.out.bam
+rm ${1}Aligned.sortedByName.out.bam
 
 samtools index -b ${1}Aligned.sortedByCoord.out.bam ${1}Aligned.sortedByCoord.out.bam.bai
 
+#java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+#REMOVE_DUPLICATES=true \
+#I=${1}Aligned.sortedByName.out.bam \
+#O=${1}Aligned.sortedByName_removeDup.out.bam \
+#M=${1}_markedDup_metrics.txt
+
 java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
 REMOVE_DUPLICATES=true \
-I=${1}Aligned.sortedByName.out.bam \
-O=${1}Aligned.sortedByName_removeDup.out.bam \
+I=${1}Aligned.sortedByCoord.out.bam \
+O=${1}Aligned.sortedByCoord_removeDup.out.bam \
 M=${1}_markedDup_metrics.txt
 
-bedtools bamtobed -i ${1}Aligned.sortedByName.out.bam > ${1}Aligned.sortedByName.out.bed
-bedtools bamtobed -i ${1}Aligned.sortedByName_removeDup.out.bam > ${1}Aligned.sortedByName_removeDup.out.bed
+samtools index -b ${1}Aligned.sortedByCoord_removeDup.out.bam ${1}Aligned.sortedByCoord_removeDup.out.bam.bai
+
+#bedtools bamtobed -i ${1}Aligned.sortedByName.out.bam > ${1}Aligned.sortedByName.out.bed
+#bedtools bamtobed -i ${1}Aligned.sortedByName_removeDup.out.bam > ${1}Aligned.sortedByName_removeDup.out.bed
+bedtools bamtobed -i ${1}Aligned.sortedByCoord.out.bam > ${1}Aligned.sortedByCoord.out.bed
+bedtools bamtobed -i ${1}Aligned.sortedByCoord_removeDup.out.bam > ${1}Aligned.sortedByCoord_removeDup.out.bed
 
 cat ../../csl_results/${7}/log/error_bowtie2.txt ${1}Log.final.out > ../../csl_results/${7}/log/error_bowtie2.txt
 
@@ -45,23 +56,32 @@ module load deepTools/3.5.2-foss-2022a
 #--effectiveGenomeSize 2913022398 \ # Mice:2150570000; GRCh38:2913022398
 ### For male mice chrX should be ignored
 
-bamCoverage -b ${1}Aligned.sortedByCoord.out.bam \
+#bamCoverage -b ${1}Aligned.sortedByCoord.out.bam \
+#--normalizeUsing RPGC \
+#--effectiveGenomeSize ${5} \
+#--binSize 10 \
+#--extendReads 200 \
+#--ignoreForNormalization chrX chrM \
+#--outFileFormat bedgraph \
+#--outFileName ${1}Aligned.sortedByCoord.out.bdg
+
+bamCoverage -b ${1}Aligned.sortedByCoord_removeDup.out.bam \
 --normalizeUsing RPGC \
 --effectiveGenomeSize ${5} \
 --binSize 10 \
---extendReads \
+--extendReads 200 \
 --ignoreForNormalization chrX chrM \
 --outFileFormat bedgraph \
---outFileName ${1}Aligned.sortedByCoord.out.bdg
+--outFileName ${1}Aligned.sortedByCoord_removeDup.out.bdg
 
-awk '( $1 ~ /^chr/ && $1 != "chrM" && $1 != "chrUn" )' ${1}Aligned.sortedByCoord.out.bdg > ${1}Aligned.sortedByCoord_filtered.out.bdg
+awk '( $1 ~ /^chr/ && $1 != "chrM" && $1 != "chrUn" )' ${1}Aligned.sortedByCoord_removeDup.out.bdg > ${1}Aligned.sortedByCoord_removeDup_filtered.out.bdg
 
-rm ${1}Aligned.sortedByCoord.out.bdg
+rm ${1}Aligned.sortedByCoord_removeDup.out.bdg
 
 # The third line is chromsize in macs2 step
 /grid/bsr/home/utama/bin/x86_64/bedGraphToBigWig \
-${1}Aligned.sortedByCoord_filtered.out.bdg \
+${1}Aligned.sortedByCoord_removeDup_filtered.out.bdg \
 ${6} \
-${1}Aligned.sortedByCoord.out.bw
+${1}Aligned.sortedByCoord_removeDup.out.bw
 
-rm ${1}Aligned.sortedByCoord_filtered.out.bdg
+rm ${1}Aligned.sortedByCoord_removeDup_filtered.out.bdg
