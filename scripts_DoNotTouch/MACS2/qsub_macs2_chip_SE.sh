22b44d739bac94663e3a30b40a5b19b14f4b56e1
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:48:15 -0400

    Update qsub_macs2_chip_SE.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/MACS2/qsub_macs2_chip_SE.sh
+++ scripts_DoNotTouch/MACS2/qsub_macs2_chip_SE.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_macs2.txt -e ../../csl_results/${7}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_chip_SE.sh $1 $2 $3 $4 $5 $6 $8
+qsub -l mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_macs2.txt -e ../../csl_results/${7}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_chip_SE.sh $1 $2 $3 $4 $5 $6 $8
 
