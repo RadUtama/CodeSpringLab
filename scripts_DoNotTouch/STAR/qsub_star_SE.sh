b78ade1fc43b242a18faec23c145a3c7dba5da48
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:50:17 -0400

    Update qsub_star_SE.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/STAR/qsub_star_SE.sh
+++ scripts_DoNotTouch/STAR/qsub_star_SE.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_star.txt -e ../../csl_results/${5}/log/error_star.txt -V ../scripts_DoNotTouch/STAR/star_SE.sh $1 $2 $3 $4
+qsub -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_star.txt -e ../../csl_results/${5}/log/error_star.txt -V ../scripts_DoNotTouch/STAR/star_SE.sh $1 $2 $3 $4
 
