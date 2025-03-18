4028570de45f3bd96342d27bf045679528ca5f1c
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:50:37 -0400

    Update qsub_bowtie2_PE.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/bowtie2/qsub_bowtie2_PE.sh
+++ scripts_DoNotTouch/bowtie2/qsub_bowtie2_PE.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=50G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_bowtie2.txt -e ${1}Log.final.out -V ../scripts_DoNotTouch/bowtie2/bowtie2_PE.sh $1 $2 $3 $4 $5 $6 $7
+qsub -l mem_free=50G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_bowtie2.txt -e ${1}Log.final.out -V ../scripts_DoNotTouch/bowtie2/bowtie2_PE.sh $1 $2 $3 $4 $5 $6 $7
