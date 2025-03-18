ddccf86e814e6f7809201474a4ab0acba468959a
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:44:59 -0400

    Update qsub_deseq2.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/DESeq2/qsub_deseq2.sh
+++ scripts_DoNotTouch/DESeq2/qsub_deseq2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${8}/log/output_deseq2.txt -e ../../csl_results/${8}/log/error_deseq2.txt -V ../scripts_DoNotTouch/DESeq2/deseq2.sh $1 $2 $3 $4 $5 $6 $7
+qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${8}/log/output_deseq2.txt -e ../../csl_results/${8}/log/error_deseq2.txt -V ../scripts_DoNotTouch/DESeq2/deseq2.sh $1 $2 $3 $4 $5 $6 $7
 
