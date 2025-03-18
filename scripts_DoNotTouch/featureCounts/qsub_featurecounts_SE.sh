be23eb9dc33e9ea7c91b6e1a9aa6a215333b6178
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:53:50 -0400

    Update qsub_featurecounts_SE.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/featureCounts/qsub_featurecounts_SE.sh
+++ scripts_DoNotTouch/featureCounts/qsub_featurecounts_SE.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-qsub -l m_mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${6}/log/output_featurecounts.txt -e ../../csl_results/${6}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_SE.sh $1 $2 $3 $4 $5
+qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${6}/log/output_featurecounts.txt -e ../../csl_results/${6}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_SE.sh $1 $2 $3 $4 $5
