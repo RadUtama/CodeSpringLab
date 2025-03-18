d79a471bb9c89d55c6101bb7f649f1c10f53a27e
Raditya Utama <proteusds@yahoo.co.id>
2025-03-18 16:46:14 -0400

    Update qsub_homer_annotag.sh: Switch to mem_free
    Switch to mem_free

--- scripts_DoNotTouch/Homer/qsub_homer_annotag.sh
+++ scripts_DoNotTouch/Homer/qsub_homer_annotag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-qsub  -l m_mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_homer_annotag.txt -e ../../csl_results/${5}/log/error_homer_annotag.txt homer_annotag.sh $1 $2 $3 $4
+qsub  -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_homer_annotag.txt -e ../../csl_results/${5}/log/error_homer_annotag.txt homer_annotag.sh $1 $2 $3 $4
 
 
