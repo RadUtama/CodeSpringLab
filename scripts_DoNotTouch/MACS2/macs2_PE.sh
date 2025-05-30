3d17f6f521d4a91d8ffda69bc413bd39a3cb1b24
Raditya Utama <proteusds@yahoo.co.id>
2025-05-30 12:08:17 -0400

    Update macs2_PE.sh: For ATAC-seq use normalized RPGC from bamCovarage in bowtie2 script instead of unnormalized treat_pileup.bdg
    For ATAC-seq use normalized RPGC from bamCovarage in bowtie2 script instead of unnormalized treat_pileup.bdg

--- scripts_DoNotTouch/MACS2/macs2_PE.sh
+++ scripts_DoNotTouch/MACS2/macs2_PE.sh
@@ -12,11 +12,11 @@ macs2 callpeak --nomodel \
 	-n ${1} --keep-dup  all \
 	--outdir ${5}
 
+## For ATAC-seq use normalized RPGC from bamCovarage in bowtie2 script instead of unnormalized treat_pileup.bdg
 /grid/bsr/home/utama/bin/x86_64/bedGraphToBigWig \
-	${5}/${1}_treat_pileup.bdg \
+	${5}/${1}Aligned.sortedByCoord_removeDup.out.bdg \
 	${4} \
-	${5}/${1}_treat_pileup.bw
-
+	${5}/${1}Aligned.sortedByCoord_removeDup.out.bw
 
 #module load Anaconda3/2023.03-1
 #conda activate deeptools
@@ -30,7 +30,7 @@ computeMatrix reference-point -p 4 \
     --referencePoint TSS \
     -b 1000 -a 1000 \
     -R ${6} \
-    -S ${5}/${1}_treat_pileup.bw \
+    -S ${5}/${1}Aligned.sortedByCoord_removeDup.out.bw \
     --skipZeros \
     -o ${5}/${1}_TSS.gz \
     --outFileSortedRegions ${5}/${1}_genes_TSS.bed
