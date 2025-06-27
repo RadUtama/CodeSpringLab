0f0cba18d247f58a31b3d8588fc0b20744481c4e
Raditya Utama <proteusds@yahoo.co.id>
2025-06-27 16:40:38 -0400

    Update qsub_macs2_PE.sh: Add $9 ${10} for homer species and out_dir
    Add $9 ${10} for homer species and out_dir

--- scripts_DoNotTouch/MACS2/qsub_macs2_PE.sh
+++ scripts_DoNotTouch/MACS2/qsub_macs2_PE.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qsub -l mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${8}/log/output_macs2.txt -e ../../csl_results/${8}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_PE.sh $1 $2 $3 $4 $5 $6 $7
+qsub -l mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${8}/log/output_macs2.txt -e ../../csl_results/${8}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_PE.sh $1 $2 $3 $4 $5 $6 $7 $9 ${10}
 
