c13355511747afa5b755835cf218a8aac673273c
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:07:08 -0500

    Update qsub_listdir.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/fastq/qsub_listdir.sh
+++ scripts_DoNotTouch/fastq/qsub_listdir.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=listDir
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=1
 
-qsub -l mem_free=1G -pe threads 1 -cwd -o ../../csl_results/${2}/log/output_listFastq.txt -e ../../csl_results/${2}log/error_listFastq.txt -V ../scripts_DoNotTouch/fastq/listdir.sh $1
+source ../scripts_DoNotTouch/fastq/listdir.sh $1
 
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 1 -cwd -o ../../csl_results/${2}/log/output_listFastq.txt -e ../../csl_results/${2}log/error_listFastq.txt -V ../scripts_DoNotTouch/fastq/listdir.sh $1
