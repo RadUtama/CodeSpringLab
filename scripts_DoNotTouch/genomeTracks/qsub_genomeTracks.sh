43de75675e36a132d596b40736f0b39f98452bbf
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:09:40 -0500

    Update qsub_genomeTracks.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/genomeTracks/qsub_genomeTracks.sh
+++ scripts_DoNotTouch/genomeTracks/qsub_genomeTracks.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=genomeTracks
+#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=4
 
-qsub -l mem_free=5G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_genomeTracks.txt -e ../../csl_results/${7}/log/error_genomeTracks.txt -V ../scripts_DoNotTouch/genomeTracks/genomeTracks.sh $1 $2 $3 $4 $5 $6
+source ../scripts_DoNotTouch/genomeTracks/genomeTracks.sh $1 $2 $3 $4 $5 $6
 
+# Ori script with grid qsub
+#qsub -l mem_free=5G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_genomeTracks.txt -e ../../csl_results/${7}/log/error_genomeTracks.txt -V ../scripts_DoNotTouch/genomeTracks/genomeTracks.sh $1 $2 $3 $4 $5 $6
