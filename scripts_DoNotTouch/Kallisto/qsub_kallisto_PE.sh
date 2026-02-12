322b5e71dca83cf608f9c9b9e2413a1e466b4e20
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:54:22 -0500

    Update qsub_kallisto_PE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/Kallisto/qsub_kallisto_PE.sh
+++ scripts_DoNotTouch/Kallisto/qsub_kallisto_PE.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=kallisto
+#SBATCH --mem-per-cpu=50G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_kallisto.txt -e ../../csl_results/${5}/log/error_kallisto.txt -V ../scripts_DoNotTouch/Kallisto/kallisto_PE.sh $1 $2 $3 $4
+source ../scripts_DoNotTouch/Kallisto/kallisto_PE.sh $1 $2 $3 $4
 
+# Ori script with grid qsub
+#qsub -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_kallisto.txt -e ../../csl_results/${5}/log/error_kallisto.txt -V ../scripts_DoNotTouch/Kallisto/kallisto_PE.sh $1 $2 $3 $4
