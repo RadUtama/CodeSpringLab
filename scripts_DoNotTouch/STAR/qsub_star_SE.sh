a893199d36116e1a71b1668ad808fff8240acac8
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:00:13 -0500

    Update qsub_star_SE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/STAR/qsub_star_SE.sh
+++ scripts_DoNotTouch/STAR/qsub_star_SE.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=star
+#SBATCH --mem-per-cpu=50G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_star.txt -e ../../csl_results/${5}/log/error_star.txt -V ../scripts_DoNotTouch/STAR/star_SE.sh $1 $2 $3 $4
+source ../scripts_DoNotTouch/STAR/star_SE.sh $1 $2 $3 $4
 
+# Ori script with grid qsub
+#qsub -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_star.txt -e ../../csl_results/${5}/log/error_star.txt -V ../scripts_DoNotTouch/STAR/star_SE.sh $1 $2 $3 $4
