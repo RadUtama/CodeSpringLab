10ac369a93c42a048ee05d25cff05858d81542f4
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:01:10 -0500

    Update qsub_bowtie2_PE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/bowtie2/qsub_bowtie2_PE.sh
+++ scripts_DoNotTouch/bowtie2/qsub_bowtie2_PE.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=bowtie2
+#SBATCH --mem-per-cpu=50G
+#SBATCH --cpus-per-task=8
+#SBATCH --export=NONE
 
-qsub -l mem_free=50G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_bowtie2.txt -e ${1}Log.final.out -V ../scripts_DoNotTouch/bowtie2/bowtie2_PE.sh $1 $2 $3 $4 $5 $6 $7
+source ../scripts_DoNotTouch/bowtie2/bowtie2_PE.sh $1 $2 $3 $4 $5 $6 $7
+
+# Ori script with grid qsub
+#qsub -l mem_free=50G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_bowtie2.txt -e ${1}Log.final.out -V ../scripts_DoNotTouch/bowtie2/bowtie2_PE.sh $1 $2 $3 $4 $5 $6 $7
