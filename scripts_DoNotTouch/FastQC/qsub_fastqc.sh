1fdfa21889fbbedb948f618b35011fa1fa205590
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:51:13 -0500

    Update qsub_fastqc.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/FastQC/qsub_fastqc.sh
+++ scripts_DoNotTouch/FastQC/qsub_fastqc.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=fastQC
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${3}/log/output_fastQC.txt -e ../../csl_results/${3}/log/error_fastQC.txt -V ../scripts_DoNotTouch/FastQC/fastqc.sh $1 $2
+source ../scripts_DoNotTouch/FastQC/fastqc.sh $1 $2
 
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${3}/log/output_fastQC.txt -e ../../csl_results/${3}/log/error_fastQC.txt -V ../scripts_DoNotTouch/FastQC/fastqc.sh $1 $2
