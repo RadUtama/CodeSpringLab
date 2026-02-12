21d63260d20a5aba6842ff72805efd36bb800d5c
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:48:45 -0500

    Update qsub_deseq2.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/DESeq2/qsub_deseq2.sh
+++ scripts_DoNotTouch/DESeq2/qsub_deseq2.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=deseq2
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${8}/log/output_deseq2.txt -e ../../csl_results/${8}/log/error_deseq2.txt -V ../scripts_DoNotTouch/DESeq2/deseq2.sh $1 $2 $3 $4 $5 $6 $7
+source ../scripts_DoNotTouch/DESeq2/deseq2.sh $1 $2 $3 $4 $5 $6 $7
 
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${8}/log/output_deseq2.txt -e ../../csl_results/${8}/log/error_deseq2.txt -V ../scripts_DoNotTouch/DESeq2/deseq2.sh $1 $2 $3 $4 $5 $6 $7
