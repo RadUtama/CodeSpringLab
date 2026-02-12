99b736cc5cbe40e41aa3306504334598f9c874a9
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:52:35 -0500

    Update qsub_homer_annotag.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/Homer/qsub_homer_annotag.sh
+++ scripts_DoNotTouch/Homer/qsub_homer_annotag.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=homer_annotag
+#SBATCH --mem-per-cpu=50G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
+source ../scripts_DoNotTouch/Homer/homer_annotag.sh $1 $2 $3 $4
 
-qsub  -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_homer_annotag.txt -e ../../csl_results/${5}/log/error_homer_annotag.txt homer_annotag.sh $1 $2 $3 $4
-
-
+# Ori script with grid qsub
+#qsub  -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${5}/log/output_homer_annotag.txt -e ../../csl_results/${5}/log/error_homer_annotag.txt ../scripts_DoNotTouch/Homer/homer_annotag.sh $1 $2 $3 $4
