730791a61738a5906bacb12dfc106921d903a1d7
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:53:26 -0500

    Update qsub_homer_diffpeak.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/Homer/qsub_homer_diffpeak.sh
+++ scripts_DoNotTouch/Homer/qsub_homer_diffpeak.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=homer_diffpeak
+#SBATCH --mem-per-cpu=50G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
+source ../scripts_DoNotTouch/Homer/homer_diffpeak.sh $1 $2 $3 $4 $5 $6
 
-qsub  -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_homer_diffpeak.txt -e ../../csl_results/${7}/log/error_homer_diffpeak.txt homer_diffpeak.sh $1 $2 $3 $4 $5 $6
-
+# Ori script with grid qsub
+#qsub  -l mem_free=50G -pe threads 4 -cwd -o ../../csl_results/${7}/log/output_homer_diffpeak.txt -e ../../csl_results/${7}/log/error_homer_diffpeak.txt ../scripts_DoNotTouch/Homer/homer_diffpeak.sh $1 $2 $3 $4 $5 $6
 
