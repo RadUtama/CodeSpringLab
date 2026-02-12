456c76e6f9070dfe460d29ac59d5c57c2fe5e83c
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:50:04 -0500

    Update qsub_diffbind.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/DiffBind/qsub_diffbind.sh
+++ scripts_DoNotTouch/DiffBind/qsub_diffbind.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=diffbind
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${9}/log/output_diffbind.txt -e ../../csl_results/${8}/log/error_diffbind.txt -V ../scripts_DoNotTouch/DiffBind/diffbind.sh $1 $2 $3 $4 $5 $6 $7 $8
+source ../scripts_DoNotTouch/DiffBind/diffbind.sh $1 $2 $3 $4 $5 $6 $7 $8
 
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${9}/log/output_diffbind.txt -e ../../csl_results/${9}/log/error_diffbind.txt -V ../scripts_DoNotTouch/DiffBind/diffbind.sh $1 $2 $3 $4 $5 $6 $7 $8
