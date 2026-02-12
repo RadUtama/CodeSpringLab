a590d3e6339278ff444671c5866d8c07d074b63f
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:05:59 -0500

    Update qsub_copy.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/fastq/qsub_copy.sh
+++ scripts_DoNotTouch/fastq/qsub_copy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=copyFastq
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=1
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 1 -cwd -o ../../csl_results/${3}/log/output_copyFastq.txt -e ../../csl_results/${3}/log/error_copyFastq.txt -V ../scripts_DoNotTouch/fastq/copy.sh $1 $2
+source ../scripts_DoNotTouch/fastq/copy.sh $1 $2 
 
-
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 1 -cwd -o ../../csl_results/${3}/log/output_copyFastq.txt -e ../../csl_results/${3}/log/error_copyFastq.txt -V ../scripts_DoNotTouch/fastq/copy.sh $1 $2
