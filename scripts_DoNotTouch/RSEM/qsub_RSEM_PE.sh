bc92df1023eed8b4999d4880d35be766fb40e87e
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:57:56 -0500

    Update qsub_RSEM_PE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/RSEM/qsub_RSEM_PE.sh
+++ scripts_DoNotTouch/RSEM/qsub_RSEM_PE.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=rsem
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=8
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_rsem.txt -e ../../csl_results/${7}/log/error_rsem.txt -V ../scripts_DoNotTouch/RSEM/RSEM_PE.sh $1 $2 $3 $4 $5 $6
+source ../scripts_DoNotTouch/RSEM/RSEM_PE.sh $1 $2 $3 $4 $5 $6
+
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_rsem.txt -e ../../csl_results/${7}/log/error_rsem.txt -V ../scripts_DoNotTouch/RSEM/RSEM_PE.sh $1 $2 $3 $4 $5 $6
