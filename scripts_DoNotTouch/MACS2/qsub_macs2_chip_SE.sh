c664122b894097aeb575e12fe4ebe6a3588fe9ea
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 16:57:11 -0500

    Update qsub_macs2_chip_SE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/MACS2/qsub_macs2_chip_SE.sh
+++ scripts_DoNotTouch/MACS2/qsub_macs2_chip_SE.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=macs2
+#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=8
+#SBATCH --export=NONE
 
-qsub -l mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_macs2.txt -e ../../csl_results/${7}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_chip_SE.sh $1 $2 $3 $4 $5 $6 $8 $9 ${10}
+source ../scripts_DoNotTouch/MACS2/macs2_chip_SE.sh $1 $2 $3 $4 $5 $6 $8 $9 ${10}
+
+# Ori script with grid qsub
+#qsub -l mem_free=5G -pe threads 8 -cwd -o ../../csl_results/${7}/log/output_macs2.txt -e ../../csl_results/${7}/log/error_macs2.txt -V ../scripts_DoNotTouch/MACS2/macs2_chip_SE.sh $1 $2 $3 $4 $5 $6 $8 $9 ${10}
