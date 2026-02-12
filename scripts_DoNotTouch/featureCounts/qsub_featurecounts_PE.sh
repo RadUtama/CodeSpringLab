f076f5374bfca59ca1b5a09dae3f93ff85c1ae45
Raditya Utama <proteusds@yahoo.co.id>
2026-02-12 17:08:03 -0500

    Update qsub_featurecounts_PE.sh: slurm convert
    slurm convert

--- scripts_DoNotTouch/featureCounts/qsub_featurecounts_PE.sh
+++ scripts_DoNotTouch/featureCounts/qsub_featurecounts_PE.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=featurecounts
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=4
+#SBATCH --export=NONE
 
-qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${6}/log/output_featurecounts.txt -e ../../csl_results/${6}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_PE.sh $1 $2 $3 $4 $5
+source ../scripts_DoNotTouch/featureCounts/featurecounts_PE.sh $1 $2 $3 $4 $5
+
+# Ori script with grid qsub
+#qsub -l mem_free=1G -pe threads 4 -cwd -o ../../csl_results/${6}/log/output_featurecounts.txt -e ../../csl_results/${6}/log/error_featurecounts.txt -V ../scripts_DoNotTouch/featureCounts/featurecounts_PE.sh $1 $2 $3 $4 $5
