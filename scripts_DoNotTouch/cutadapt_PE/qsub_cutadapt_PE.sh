ccf6d5c6b790669f1d0bf7c00c357a76e8ca0d0a
Raditya Utama <proteusds@yahoo.co.id>
2026-03-23 12:20:18 -0400

    Update qsub_cutadapt_PE.sh: #SBATCH --time=2-00:00:00
    #SBATCH --time=2-00:00:00

--- scripts_DoNotTouch/cutadapt_PE/qsub_cutadapt_PE.sh
+++ scripts_DoNotTouch/cutadapt_PE/qsub_cutadapt_PE.sh
@@ -3,6 +3,7 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=4
 #SBATCH --export=NONE
+#SBATCH --time=2-00:00:00
 
 source ../scripts_DoNotTouch/cutadapt_PE/cutadapt_PE.sh $1 $2 $3 $4 $5 $6 $7
 
