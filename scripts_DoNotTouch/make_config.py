d68fb25c6cfc9c929498ce81f6f8e29f50f7e504
Raditya Utama <proteusds@yahoo.co.id>
2026-03-23 10:44:19 -0400

    Update make_config.py: prompts to give results path other than home
    prompts to give results path other than home

--- scripts_DoNotTouch/make_config.py
+++ scripts_DoNotTouch/make_config.py
@@ -1,5 +1,6 @@
 
 def config():
+    
     print("Do you want to use your most recent project name, genome, design matrix, and reads folders:(e.g y/n)")
     print("\033[91m"+"If this is your first time, type"+"\033[94m"+" n"+"\x1b[0m")
     param = input()
@@ -8,9 +9,20 @@ def config():
         print("Provide any unique project name:")
         print("\033[91m"+"If you want to use our example dataset, type"+"\033[94m"+" example_dataset"+"\x1b[0m")
         project_name = input()
+
+        print("========================================")
+        print("Provide a path/location where to store your analysis results:(Sometimes your home has limited space)")
+        print("\033[91m"+"If not sure, leave it blank and press enter. Results will be stored in the same location"+"\033[94m")
+        res_dir = input()
+        if len(res_dir) == 0:
+            res_dir = "../../csl_results/"
+        else:
+            res_dir = res_dir+"/"
+        
         conf = open("../scripts_DoNotTouch/config.py", "w")
         conf.write("project_name="+"'"+project_name+"'"+"\n")
         conf.write("parameters_exist="+"'"+param+"'"+"\n")
+        conf.write("results_directory="+"'"+res_dir+"'"+"\n")
         conf.close()
     else:
         with open('../scripts_DoNotTouch/config.py', 'r') as file:
