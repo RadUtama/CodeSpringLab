1da9ff2bd5850111a01751806ec9dabee829468c
Raditya Utama <proteusds@yahoo.co.id>
2026-03-23 12:16:56 -0400

    Update qsub_macs2_PE.sh: #SBATCH --time=2-00:00:00
    #SBATCH --time=2-00:00:00

--- scripts_DoNotTouch/MACS2/qsub_macs2_PE.sh
+++ scripts_DoNotTouch/MACS2/qsub_macs2_PE.sh
@@ -3,6 +3,7 @@
 #SBATCH --mem-per-cpu=5G
 #SBATCH --cpus-per-task=8
 #SBATCH --export=NONE
+#SBATCH --time=2-00:00:00
 
 source ../scripts_DoNotTouch/MACS2/macs2_PE.sh $1 $2 $3 $4 $5 $6 $7 $9 ${10}
 
